5c46adf929dca85f79c2426884ab4b455e414b71
yahayagodiwa <yahayagodiwa8@gmail.com>
2025-05-19 14:29:57 +0100

    try catch added

--- controller/authController.js
+++ controller/authController.js
@@ -262,11 +262,15 @@ const updateProfile = async (req, res)=>{
 //////--------------------------------- User Dashboard -------------------------------//////////////////
 
 const userDashboard = async (req, res)=>{
-  const userId = await req.user._id
+  try {
+    const userId = await req.user._id
   if(!userId) return res.status(404).json({error: "User not found"})
     const user = await User.findById(userId).populate('borrows')
-  
+  if(!user) return res.status(404).json({error: "user not found"})
   return res.status(200).json({user})
+  } catch (error) {
+    return res.status(500).json({error: "Error updating user" + error.message})
+  }
 }
 
 
