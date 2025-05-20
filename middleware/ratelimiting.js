de7d53a7eade21e10524f7033348dcf1853f9640
yahayagodiwa <yahayagodiwa8@gmail.com>
2025-05-20 15:57:44 +0100

    rate limiting increased

--- middleware/ratelimiting.js
+++ middleware/ratelimiting.js
@@ -3,7 +3,7 @@ const { rateLimit } = require('express-rate-limit');
 
 const limiter =  rateLimit({
     windowMs: 15 * 60 * 1000,
-    limit: 5,
+    limit: 100,
     standardHeaders: 'draft-8',
     legacyHeaders: false,
 })
